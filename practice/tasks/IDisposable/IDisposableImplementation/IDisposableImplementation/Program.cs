9c441e208a333e54c2c39af4c59bfe3a034b4f1d
Mikhail Kozlov <mestremi6a@gmail.com>
2017-06-07 23:57:23 +0300

    Refactored TimeLogger method to track time for execution

--- practice/tasks/IDisposable/IDisposableImplementation/IDisposableImplementation/Program.cs
+++ practice/tasks/IDisposable/IDisposableImplementation/IDisposableImplementation/Program.cs
@@ -8,11 +8,11 @@ namespace NetMentoring
     {
         private static void Main(string[] args)
         {
-            DisposingLoggerAndTarget();
+            TimeLogger(DisposingLoggerAndTarget);
 
-            DisposingOnlyTarget();
+            TimeLogger(DisposingOnlyTarget);
 
-            DisposingLoggerAndTargetInsideLoop();
+            TimeLogger(DisposingLoggerAndTargetInsideLoop);
 
             Console.WriteLine("Finished");
             Console.ReadKey();
@@ -20,56 +20,44 @@ namespace NetMentoring
 
         private static void DisposingLoggerAndTargetInsideLoop()
         {
-            TimeLogger(nameof(DisposingLoggerAndTargetInsideLoop), () =>
-            {
-                for (var i = 0; i < 10000; i++)
-                {
-                    using (var file = new FileStream(@"\log.txt", FileMode.OpenOrCreate | FileMode.Append))
-                    using (var logger = new MemoryStreamLogger(file))
-                    {
-                        logger.Log($"Interation number #{i}");
-                    }
-                }
-            });
-        }
-
-        private static void DisposingOnlyTarget()
-        {
-            TimeLogger(nameof(DisposingOnlyTarget), () =>
-            {
-                using (var file = new FileStream(@"\log.txt", FileMode.OpenOrCreate | FileMode.Append))
-                {
-                    for (var i = 0; i < 10000; i++)
-                    {
-                        WriteLog(file, $"Interation number #{i}");
-                    }
-                }
-
-                void WriteLog(Stream to, string text)
-                {
-                    var logger = new MemoryStreamLogger(to);
-                    logger.Log(text);
-                }
-            });
-        }
-
-        private static void DisposingLoggerAndTarget()
-        {
-            TimeLogger(nameof(DisposingLoggerAndTarget), () =>
+            for (var i = 0; i < 10000; i++)
             {
                 using (var file = new FileStream(@"\log.txt", FileMode.OpenOrCreate | FileMode.Append))
                 using (var logger = new MemoryStreamLogger(file))
                 {
-                    for (var i = 0; i < 10000; i++)
-                    {
-                        logger.Log($"Interation number #{i}");
-                    }
+                    logger.Log($"Interation number #{i}");
                 }
-            });
+            }
         }
 
-        private static void TimeLogger(string actionName, Action action)
+        private static void DisposingOnlyTarget()
         {
+            using (var file = new FileStream(@"\log.txt", FileMode.OpenOrCreate | FileMode.Append))
+            {
+                for (var i = 0; i < 10000; i++)
+                {
+                    var logger = new MemoryStreamLogger(file);
+                    logger.Log($"Interation number #{i}");
+                }
+            }
+        }
+
+        private static void DisposingLoggerAndTarget()
+        {
+            using (var file = new FileStream(@"\log.txt", FileMode.OpenOrCreate | FileMode.Append))
+            using (var logger = new MemoryStreamLogger(file))
+            {
+                for (var i = 0; i < 10000; i++)
+                {
+                    logger.Log($"Interation number #{i}");
+                }
+            }
+        }
+
+        private static void TimeLogger(Action action)
+        {
+            var actionName = action.Method.Name;
+
             using (var file = File.CreateText($"{actionName}-log.txt"))
             {
                 var t = new Stopwatch();
